67fae98e07e9dae008084a63dc1640f3e515b90d
Daniel Washington <danielvertigo@hotmail.com>
2021-06-23 16:51:28 -0300

    files

--- ReversetheCase/ViewController.swift
+++ ReversetheCase/ViewController.swift
@@ -5,6 +5,18 @@
 //  Created by Daniel Washington Ignacio on 23/06/21.
 //
 
+/*
+ Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+ Examples
+
+ reverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+ reverseCase("MANY THANKS") ➞ "many thanks"
+
+ reverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +24,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.reverseCase("Happy Birthday"))
+        print(self.reverseCase("MANY THANKS"))
+        print(self.reverseCase("sPoNtAnEoUs"))
     }
 
+    func reverseCase(_ str: String) -> String {
+        var arr: [String] = []
+        for n in str{
+            if n.isLowercase{
+                arr.append(n.uppercased())
+            }else{
+                arr.append(n.lowercased())
+            }
+        }
+        
+        return arr.joined()
+    }
 
 }
 
